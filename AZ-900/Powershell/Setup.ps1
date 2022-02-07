e2261c43ef20caa77205d9fceeb6bbe2d20c38cc
John Snow <66561475+PowerOps-MK@users.noreply.github.com>
2022-02-07 23:40:35 +0100

    Update Setup.ps1

--- AZ-900/Powershell/Setup.ps1
+++ AZ-900/Powershell/Setup.ps1
@@ -1,2 +1,8 @@
+Install-Module az -Scope AllUsers -Force
+Install-Module az.resourcegraph -Scope AllUsers -Force
+Install-Module aztable -Scope AllUsers -Force
+Install-Module microsoft.graph -Scope AllUsers -Force
+Install-Module azureadpreview -Scope AllUsers -Force
+
 Connect-AzAccount
 
