26ac201a708392e027d49dc1d8b9c30b396d9f99
John Snow <66561475+PowerOps-MK@users.noreply.github.com>
2022-02-13 18:17:26 +0100

    Update CloudShell.sh

--- AZ-900/Demos/CloudShell.sh
+++ AZ-900/Demos/CloudShell.sh
@@ -1,6 +1,7 @@
 # Create resourcegroup for CLI en ARM
 az group create --name "RG-AZ900-CLI" --location "westeurope"
 az group create --name "RG-AZ900-ARM" --location "westeurope"
+az group create --name "RG-AZ900-Portal" --location "westeurope"
 
 # List resourcegroups
 az group list --query "[].{name:name}" --output table
