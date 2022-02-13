7d361cc9d8824bacf09d74539c11c658a7515aa6
John Snow <66561475+PowerOps-MK@users.noreply.github.com>
2022-02-13 17:08:16 +0100

    Update CloudShell.sh

--- AZ-900/Demos/CloudShell.sh
+++ AZ-900/Demos/CloudShell.sh
@@ -3,7 +3,7 @@ az group create --name "RG-AZ900-CLI" --location "westeurope"
 az group create --name "RG-AZ900-ARM" --location "westeurope"
 
 # List resourcegroups
-az group list
+az group list --query "[].{name}" --output table
 
 # Create new storageaccount
 az storage account create --name "mkaz900demo" --resource-group "RG-AZ900-CLI" --location "westeurope" --sku "Standard_LRS"
